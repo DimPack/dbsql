af946d9fe481ba08da9f73d2ddb34dc6834bec1a
Dima Prosto <csourgg@gmail.com>
2024-08-19 10:42:05 +0300

    add m to n

--- primaryKey.sql
+++ primaryKey.sql
@@ -1,15 +1,15 @@
 DROP TABLE IF EXISTS "messages";
 CREATE TABLE IF NOT EXISTS "messages"(
-    "id" BIGSERIAL PRIMARY KEY,
+    "id" bigserial PRIMARY KEY,
     "body" VARCHAR(2048) NOT NULL CHECK("body" != ''),
-    "author" VARCHAR(32) NOT NULL CHECK("author" != ''),
+    "authorId" INT REFERENCES "users"("id"),
     "createdAt" TIMESTAMP NOT NULL DEFAULT current_timestamp,
     "isRead" BOOLEAN DEFAULT false
 );
 
-INSERT INTO "messages"("body", "author")
+INSERT INTO "messages"("body", "authorId")
 VaLUES
-('hi!', 'Tom'),
-('hi!', 'Tom'),
-('Brad', 'hello'),
-('))))', 'Alex');
+('hi!', 2),
+('hi!', 1),
+('Brad', 4),
+('))))', 3);
